b529da013b60c7c6d46c389c157395b140c93bc7
chuoneone <90313941+chuoneone@users.noreply.github.com>
2026-01-08 19:37:19 +0800

    Create sw_learning.js

---
+++ sw_learning.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'sped-learning-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './responsive.css',
+  './images/dog.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
